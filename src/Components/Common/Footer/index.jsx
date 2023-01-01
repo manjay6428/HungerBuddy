866229ccb64ce1a3fdf8b29b5aff0c59fb83e529
Manjay <manjay6428@gmail.com>
2023-01-02 00:17:01 +0530

    Added test

--- src/Components/Common/Footer/index.jsx
+++ src/Components/Common/Footer/index.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer>
       <p>We're a team of professional cooks who are exited about their food, amazing skills and experience and expertise in cooking</p>
-      Developed by-Manjay Kumar
+      Developed by-Manjay Kumar!
       <Logo/>
     </footer>
   )
